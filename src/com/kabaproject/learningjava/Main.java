c84be96277028ce5b82a58984887454fe7a915bf
MarioHdez <mariohdez.developer@gmail.com>
2022-06-28 01:27:19 -0500

    Basic Data Types

---
+++ src/com/kabaproject/learningjava/Main.java
@@ -0,0 +1,16 @@
+package com.kabaproject.learningjava;
+
+public class Main {
+    public static void main(String[] args){
+        // Primary Data Types
+        boolean isAdult = true; // 1 bit (true|false)
+        byte age = 21; // 1 byte [-128, 127]
+        short year = 2022; // 2 bytes [-32'768, 32'767]
+        int id = 12054602; // 4 bytes [-2B, 2B]
+        long uuid1 = 156_489_025; // 8 bytes [-9Q, 9Q]
+        float PI = 3.141592f; // 4 bytes 6-7 digits
+        double height = 1.58; // 8 bytes up to 15 digits
+        char sex = 'M'; // 2 bytes S.Char/Letter/ASCII
+        String name = "Mario Hernandez"; // Varies A sequence of characters
+    }
+}
