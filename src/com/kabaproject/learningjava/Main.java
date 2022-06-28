83f98c026a3b5a2d5d00e4bd40fd551176c9edd5
MarioHdez <mariohdez.developer@gmail.com>
2022-06-28 01:50:18 -0500

    User Input with Scanners

--- src/com/kabaproject/learningjava/Main.java
+++ src/com/kabaproject/learningjava/Main.java
@@ -1,16 +1,17 @@
 package com.kabaproject.learningjava;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        // Primary Data Types
-        boolean isAdult = true; // 1 bit (true|false)
-        byte age = 21; // 1 byte [-128, 127]
-        short year = 2022; // 2 bytes [-32'768, 32'767]
-        int id = 12054602; // 4 bytes [-2B, 2B]
-        long uuid1 = 156_489_025; // 8 bytes [-9Q, 9Q]
-        float PI = 3.141592f; // 4 bytes 6-7 digits
-        double height = 1.58; // 8 bytes up to 15 digits
-        char sex = 'M'; // 2 bytes S.Char/Letter/ASCII
-        String name = "Mario Hernandez"; // Varies A sequence of characters
+        // User Input
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+        System.out.println("How old are you?");
+        short age = scanner.nextShort();
+
+        System.out.println("Your name is " + name + " and you're " + age);
     }
 }
