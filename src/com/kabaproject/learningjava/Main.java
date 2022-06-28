71cfd1dcad903fbf9c34cfba079d35956b6cdd9d
MarioHdez <mariohdez.developer@gmail.com>
2022-06-28 02:10:04 -0500

    GUI Intro

--- src/com/kabaproject/learningjava/Main.java
+++ src/com/kabaproject/learningjava/Main.java
@@ -1,17 +1,14 @@
 package com.kabaproject.learningjava;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args){
-        // User Input
-        Scanner scanner = new Scanner(System.in);
+        // GUI intro
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello " + name);
 
-        System.out.println("What is your name?");
-        String name = scanner.nextLine();
-        System.out.println("How old are you?");
-        short age = scanner.nextShort();
-
-        System.out.println("Your name is " + name + " and you're " + age);
+        short age = Short.parseShort(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "Your " + age);
     }
 }
