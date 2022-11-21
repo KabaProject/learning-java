204807bf0605cc03823ca663c4f79d7708a7cabb
MarioHdez <mariohdez.developer@gmail.com>
2022-11-21 05:08:27 -0600

    Singleton Pattern

--- src/com/kabaproject/learningjava/Main.java
+++ src/com/kabaproject/learningjava/Main.java
@@ -2,28 +2,11 @@ package com.kabaproject.learningjava;
 
 public class Main {
     public static void main(String[] args){
-        // Concurrency
-        Thread threadOne = new Thread(threadBehavior());
-        Thread threadTwo = new Thread(threadBehavior());
+        // Singleton
+        Database db = Database.getInstance("music");
+        db = Database.getInstance("papa");
 
-        threadOne.start();
-        threadTwo.start();
-    }
-
-    public static Runnable threadBehavior (){
-        return new Runnable(){
-            @Override
-            public void run(){
-                for(int x = 1; x <= 10; x++){
-                    System.out.println("Lap number: " + x);
-                    try{
-                        Thread.sleep(1000);
-                    }catch (InterruptedException e){
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
+        System.out.println(db.getName());
     }
 }
 
